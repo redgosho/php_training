7e52b82a3b7acf51492a70862892e174bb179fc6
akasananosa <akasananosa@gmail.com>
2019-05-05 16:23:49 +0900

    a

--- 2019-04-30/pages/top.php
+++ 2019-04-30/pages/top.php
@@ -1,32 +1,32 @@
 <div id="contentContainer">
   <!-- slide 1 -->
   <section class="parallax" id="slide1">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_01.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_01.php'); ?>
   </section>
   <!-- slide 2 -->
   <section class="parallax" id="slide2">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_01.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_01.php'); ?>
   </section>
   <!-- slide 3 -->
   <section class="parallax" id="slide3">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_02.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_02.php'); ?>
   </section>
   <!-- slide 4 -->
   <section class="parallax" id="slide4">
 
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_03.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_03.php'); ?>
   </section>
   <!-- slide 5 -->
   <section class="parallax" id="slide5">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_04.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_04.php'); ?>
   </section>
   <!-- slide 6 -->
   <section class="parallax" id="slide6">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_05.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_05.php'); ?>
   </section>
   <!-- slide 7 -->
   <section class="parallax" id="slide7">
-    <?php include(dirname(__FILE__).'../tasks_custom/190430_php_05.php'); ?>
+    <?php include(dirname(__FILE__).'./tasks_custom/190430_php_05.php'); ?>
   </section>
   
   <div id="pagerNavi">
