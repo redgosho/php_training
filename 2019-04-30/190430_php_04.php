02782fa393148a006b727ca476e9bef2618b1671
akasananosa <akasananosa@gmail.com>
2019-05-05 13:46:03 +0900

    comp

---
+++ 2019-04-30/190430_php_04.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>人口データ検索システム</h1>
+    <div>
+        <form method="post" action="./190430_php_04_result.php">
+            <?php
+                $input = <<<EOM
+                <p>県コード
+                    <input type="text" id="kenCode" name="kenCode[]" requiredminlength="4" maxlength="8" size="10" placeholder="県コードを入力">
+                </p>
+EOM;
+                for ($i=0; $i < 5; $i++) { 
+                    echo $input;
+                }
+            ?>
+
+            <p>
+                <input type="submit" value="検索">
+            </p>
+        </form>
+    </div>
+</body>
+</html>
