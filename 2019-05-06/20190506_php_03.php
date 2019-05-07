f2142ce1672aafed6e7e9afe74811a62083f7cfe
GoshoAkazawa <akazawa-g@asahi.com>
2019-05-07 17:52:10 +0900

    完成GMTだった

--- 2019-05-06/20190506_php_03.php
+++ 2019-05-06/20190506_php_03.php
@@ -1,4 +1,5 @@
 <?php
+main();
 function main () {
     $rintenki[10] = array(100000,"10:00");
     $rintenki[20] = array(100000,"10:15");
@@ -6,22 +7,22 @@ function main () {
     $rintenki[40] = array(80000,"10:00");
     
     $nowTime = "11:30";
-    $nowTime_int = strtotime($nowTime);
     
     echo "部数表示システム\n";
     echo "現在時刻は{$nowTime}です。\n";
     for ($i=10; $i<50;$i+=10){
-        $startTime_int = strtotime($rintenki[$i][1]);
-        $NumCopys = countCopys($nowTime_int,$startTime_int);
+        $NumCopys = countCopys($rintenki[$i][1],$nowTime,$rintenki[$i][0]);
         echo "F{$i}の印刷開始時刻は{$rintenki[$i][1]}で、現在の印刷部数は{$NumCopys}です。\n";
     }
 }
-function countCopys ($nowTime_int,$startTime_int) {
-    $gap = $nowTime_int - $startTime_int;
-    if ($gap>0) {
-        return date("i");
+function countCopys ($start,$end,$sheetNum_m) {//時間の差異を出して一分の生産数をかけて、プリント枚数を数える。なおGMT
+    $startSec = strtotime($start);
+    $endSec   = strtotime($end);
+    $diff = $endSec - $startSec;
+    if ($diff>0) {
+        $minites = gmdate("H",$diff) * 60 + gmdate("i",$diff);
+        $sheetNum = $minites * $sheetNum_m;
+        return $sheetNum;
     }
 }
-
-
 ?>
