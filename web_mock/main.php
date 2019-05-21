0aac42eee9acdc8439d033aa0db3aad31ca8c975
akasananosa <akasananosa@gmail.com>
2019-05-22 00:51:32 +0900

    add mock

---
+++ web_mock/main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>main</title>
+</head>
+<body>
+    <main id="root">
+        <div class="page_bg"></div>
+        <div class="page_deco"></div>
+        <div class="page_frame"></div>
+        <div class="page_inner">
+            <div class="title">
+                <h2>★たのしいサイト★</h2>
+            </div>
+            <div class="articleCard">
+                <div class="card_title">
+                    <h2>title</h2>
+                    <p>2019-05-12</p>
+                </div>
+                <div class="card_text">
+                    <p>こんにちはこれはテストです。さようならみんな</p>
+                </div>
+            </div>
+            <div class="articleCard">
+                <div class="card_title">
+                    <h2>title</h2>
+                    <p>2019-05-12</p>
+                </div>
+                <div class="card_text">
+                    <p>こんにちはこれはテストです。さようならみんな</p>
+                </div>
+            </div>
+            <div class="word">
+                <a href="#top">トップにもどる！</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
